0ab76897b7805ad3b60515360724745cfbc43e37
huangzhe0809 <654907639@qq.com>
2018-10-07 12:00:48 +0800

    Initial commit

---
+++ src/com/atguigu/java/TemplatesTest1.java
@@ -0,0 +1,42 @@
+package com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @date 2018/10/6 - 21:15
+ */
+public class TemplatesTest1 {
+
+    // 生成的模板的演示2
+    /**
+     * 客户的 id
+     */
+    private int id;
+    
+    /**
+     * 客户的姓名
+     */
+     private String name;
+     
+
+
+
+
+    public static final int NUM = 1;
+
+    // 修改2
+    public static final int NUM1 = 10;
+
+
+    // 修改1：psvm -> main
+    public static void main(String[] args) {
+
+    }
+
+
+    // 生成模板的演示
+    public void testUpdate() {
+        // 修改的测试
+    }
+
+
+}
---
+++ src/com/atguigu/java/t1.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @date 2018/10/6 - 13:03
+ */
+public class t1 {
+}
